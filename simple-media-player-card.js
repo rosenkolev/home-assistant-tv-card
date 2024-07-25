992ce388c7269056e0b1dd896479ddde19bba83c
Rosen <rosinto.kolev@gmail.com>
2024-07-25 14:19:22 +0300

    Create simple-media-player-card.js

---
+++ simple-media-player-card.js
@@ -0,0 +1,271 @@
+import { ScopedRegistryHost } from 'https://unpkg.com/@lit-labs/scoped-registry-mixin@1.0.3/scoped-registry-mixin.js?module';
+import {
+  LitElement,
+  html,
+  css,
+  nothing
+} from "https://unpkg.com/lit-element@3.3.3/lit-element.js?module";
+
+import { 
+  computeDomain,
+  hasConfigOrEntityChanged,
+  fireEvent
+} from 'https://unpkg.com/custom-card-helpers@1.9.0/dist/index.m.js?module';
+
+class MediaPlayerCardService {
+  /** @type {MediaPlayerState} */
+  state;
+  setHass(hass, entity) {
+    this.hass = hass;
+    this.state = hass.states[entity];
+    this.entityId = entity;
+  }
+
+  get name() { return this.state.attributes.friendly_name; }
+  get volumeEnabled() { return this.#isFeatureOn(4); } // VOLUME_SET: 4
+  get muteEnabled() { return this.#isFeatureOn(8); }   // VOLUME_MUTE: 8
+  get powerEnabled() { return this.#isFeatureOn(128); } // TURN_ON: 128, TURN_OFF: 256
+  get sourceEnabled() { return this.#isFeatureOn(2048); } // SELECT_SOURCE: 2048
+  get isOn() { return this.state.state === 'on'; }
+  get isOff() { return this.state.state === 'off'; }
+  get volume() { return this.state.attributes.volume_level * 100 }
+  get isMuted() { return this.state.attributes.is_volume_muted; }
+  get isMutedIcon() { return this.isMuted ? 'mdi:volume-off' : 'mdi:volume-high' }
+  get sources() { return this.state.attributes.source_list; }
+  get device() { return this.state.attributes.device_class; }
+  get deviceIcon() { return this.device === 'tv' ? 'mdi:television' : 'mdi:cast-variant' }
+  get stateIcon() { return this.state.state === 'off' ? 'mdi:power-plug-off-outline' : this.state.state === 'idle' ? 'mdi:power-sleep' : 'mdi:power'; }
+
+  togglePower() { 
+    if (this.isOff) {
+      return this.#call('turn_on');
+    } else {
+      return this.#call('turn_off');
+    }
+  }
+
+  toggleMute() { return this.#call('volume_mute', { is_volume_muted: !this.isMuted }); }
+  volumeUp() { return this.#call("volume_up"); }
+  volumeDown() { return this.#call("volume_down"); }
+  selectSource(source) { return this.#call("select_source", { source }); }
+  
+  #isFeatureOn = (val) => (this.state.attributes.supported_features & val) === val;
+  #call = (name, data) => this.hass.callService('media_player', name, { entity_id: this.entityId, ...data });
+}
+
+/** @property {MediaPlayerCardService} #service */
+class MediaPlayerCard extends LitElement {
+  static supportedDomains = ['media_player'];
+  static get properties() {
+    return {
+      hass: { attribute: false },
+      config: { state: true }
+    };
+  }
+
+  static getConfigElement() {
+    return document.createElement('simple-media-player-card-editor');
+  }
+
+  static getStubConfig() {
+    return { entity: "media_player.samsung_tv" };
+  }
+
+  #service = new MediaPlayerCardService();
+
+  setConfig(config) {
+    if (!config || !config.entity) {
+      throw new Error("You need to define an entity");
+    }
+
+    const domain = computeDomain(config.entity);
+    if (!MediaPlayerCard.supportedDomains.includes(domain)) {
+      throw new Error(`The domain ${domain} is not supported!`);
+    }
+
+    this.config = { ...config, domain };
+  }
+
+  /** Get the card height (x * 50px) (e.g. 3 is 150px). */
+  getCardSize() { return 1; }
+
+  shouldUpdate(changedProps) {
+    return this.config && this.hass && hasConfigOrEntityChanged(this, changedProps, false);
+  }
+
+  update(changedProperties) {
+    // reset the service properties.
+    if (changedProperties.has('hass') && this.config) {
+      this.#service.setHass(this.hass, this.config.entity);
+    }
+
+    super.update(changedProperties);
+  }
+
+  render() {
+    if (!this.config || !this.hass) {
+      return nothing;
+    }
+
+    const _ = this.#service;
+    return html`
+      <ha-card>
+        <div class="content">
+          <div class="toolbar">
+            <span title=${_.device}">
+              <ha-icon class="device-icon"
+                       .icon="${_.deviceIcon}"></ha-icon>
+            </span>
+            <div class="name">${_.name}</div>
+            <span title=${_.state.state}>
+              <ha-icon class="power-icon"
+                       .icon="${_.stateIcon}"></ha-icon>
+            </span>
+            <div class="toolbar-actions">
+              ${_.isOn ? html`
+                <ha-icon class="mute-icon"
+                        .icon="${_.isMutedIcon}"
+                        ?disabled=${!_.muteEnabled}
+                        @click=${() => _.toggleMute()}></ha-icon>
+                <ha-icon class="state-icon"
+                         ?disabled=${!_.volumeEnabled}
+                         icon="mdi:volume-minus"
+                         @click=${() => _.volumeUp()}></ha-icon>
+                <div class="volume-level">${_.volume}</div>
+                <ha-icon class="state-icon"
+                         ?disabled=${!_.volumeEnabled}
+                         icon="mdi:volume-plus"
+                         @click=${() => _.volumeDown()}></ha-icon>
+              `: ''}
+              <ha-icon class="power-button"
+                       icon="mdi:power"
+                       @click=${(ev) => _.togglePower()}></ha-icon>
+            </div>
+          </div>
+        </div>
+      </ha-card>
+    `;
+  }
+
+  // styles
+  static get styles() {
+    return css`
+      .content { padding: 2px 10px; }
+      
+      .toolbar {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+      }
+      
+      .power-icon {
+        opacity: 0.8;
+        --mdc-icon-size: 20px;
+      }
+
+      .toolbar-actions {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+        gap: 10px;
+        align-items: center;
+      }
+      .toolbar-actions > ha-icon {
+        cursor: pointer;
+        --mdc-icon-size: 28px;
+      }
+      ha-icon:disabled {
+        opacity: 0.5;
+      }
+    `;
+  }
+}
+
+class MediaPlayerCardEditor extends ScopedRegistryHost(LitElement) {
+  static elementDefinitions = {};
+  static get properties() {
+    return {
+      hass: { attribute: false },
+      config: { state: true }
+    };
+  }
+
+  setConfig(config) {
+    this.config = config;
+  }
+
+  firstUpdated() {
+    MediaPlayerCardEditor.#loadHomeAssistantComponents(this, ['ha-entity-picker']);
+  }
+
+  get _entity() {
+    return this.config?.entity || '';
+  }
+
+  render() {
+    if (!this.config || !this.hass) {
+      return nothing;
+    }
+
+    return html`
+    <div class="container">
+      <ha-entity-picker
+        .hass=${this.hass}
+        .includeDomains=${MediaPlayerCard.supportedDomains}
+        .value=${this._entity}
+        .configValue=${'entity'}
+        .required=${true}
+        @change=${this._valueChangedEntity}
+      ></ha-entity-picker>
+    </div>
+    `;
+  }
+
+  _valueChangedEntity(ev) {
+    const value =  ev.target?.value;
+    if (value && this.config && this.hass) {
+      const config = { ...this.config, entity: value };
+      fireEvent(this, 'config-changed', { config });
+    }
+  }
+
+  static async #loadHomeAssistantComponents(element, components) {
+    const registry = element.shadowRoot?.customElements;
+    if (registry) {
+      const toBeLoaded = components.filter(x => !registry.get(x));
+      if (toBeLoaded.length > 0) {
+        const ch = await window.loadCardHelpers();
+        const c = await ch.createCardElement({ type: "entities", entities: [] });
+        await c.constructor.getConfigElement();
+
+        toBeLoaded.forEach((component) => (registry.define(component, window.customElements.get(component))));
+      }
+    }
+  }
+}
+
+customElements.define("simple-media-player-card", MediaPlayerCard);
+customElements.define("simple-media-player-card-editor", MediaPlayerCardEditor);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: "simple-media-player-card",
+  name: "Simple Media Player Card",
+  description: "A simple card to manage media player on/off and volume!",
+  //documentationURL: "https://developers.home-assistant.io/docs/frontend/custom-ui/custom-card/",
+});
+
+/**
+ * @typedef {{
+ *   entity_id: string,
+ *   state: string,
+ *   attributes: {
+ *     device_class: string,
+ *     friendly_name: string,
+ *     is_volume_muted: boolean,
+ *     source_list: string[],
+ *     supported_features: number,
+ *     volume_level: number
+ *   }
+ * }} MediaPlayerState
+ */
